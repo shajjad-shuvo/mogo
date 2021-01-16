957449f64439b5ec147bfc4ff4309ca39381140e
shajjad-shuvo <shajjad.shuvo84@gmail.com>
2021-01-16 18:12:28 +0600

    stick nav with js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(function () {
     'use strict';
+    
+    
+   $(window).on('scroll',function(){
+       var scrolling = $(this).scrollTop();
+        
+        if(scrolling  > 1020){
+            $('.navbar').addClass('sticky-nav');
+        }else{
+            $('.navbar').removeClass('sticky-nav');
+        }
+    });
     // banner slider;
     $('.banner-slider').slick({
         infinite: true,
