49ecd715aed7661f961c48d8a5cbd3a9c845d3ca
shajjad-shuvo <shajjad.shuvo84@gmail.com>
2021-01-16 15:41:21 +0600

    banner css done with slick

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    // banner slider;
+    $('.banner-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay : true,
+        dots : true,
+    });
+});
